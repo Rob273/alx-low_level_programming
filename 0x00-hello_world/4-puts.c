a103caeef9957e2b735815f62c8c81266e661fb5
Rob <robelmidekso@gmail.com>
2023-02-16 06:10:52 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main - this is main function
+* description - main uses puts function
+* return: 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
