560d467d891d48c2fd5863f8e236cfe9f595dc34
Rob <robelmidekso@gmail.com>
2023-02-16 09:21:11 -0800

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float e;
+        char a;
+        int b;
+        long int c;
+        long long int d;
+        float e;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof((a);
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof((b));
@@ -18,5 +18,4 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof((d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof((e));
 return (0);
 }
-
 
