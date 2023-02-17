1ec00c7bc864815617e9f154e8a8e6f0a2efbcf1
Rob <robelmidekso@gmail.com>
2023-02-17 11:41:40 -0800

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - lower abc...
+ * Return: 0
+ */
+int main(void)
+{
+char alpha;
+
+        for (alpha = 'z'; alpha >= 'a'; alpha--)
+                putchar(alpha);
+
+        putchar('\n');
+
+        return (0);
+}
+
