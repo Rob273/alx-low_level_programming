d102692d0a673888c1f0a6050a053582f0068897
rob <robelmidedso@gmail.com>
2023-02-21 15:29:49 +0000

    put

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void);
+{
+char c;
+int r;
+for (r = 0; r < 10; r++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+_putchar('\n');
+}
+}
