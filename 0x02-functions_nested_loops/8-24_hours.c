a537c53470634698c84d0dd16806a89dffb99eff
rob <robelmidedso@gmail.com>
2023-02-21 19:36:57 +0000

    put

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,16 +5,28 @@
  */
 void jack_bauer(void)
 {
-	int c;
-	int r;
-for (c = 0; c <= 23; c++)
+	int a, b, c, d;
+for (a = 0; a <= 2; a++)
 {
-_putchar(c);
+	for (b = 0; b <= 9; b++)
+	{
+		if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+		{
+		for (c = 0; c <= 5; c++)
+		{
+			for (d = 0; d <= 9; d++)
+			{
+_putchar(a + '0');
+_putchar(b + '0');
 _putchar(58);
-for (r = 0; r <= 59; r++)
-{
-_putchar(r);
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+			}
+		}
+		}
+	}
 }
-}
+
 }
 
