e7284b2dd111f751d4cdff5c9445d21442d09367
rob <robelmidedso@gmail.com>
2023-02-23 18:21:57 +0000

    c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_number -prints intiger
+ * @n: intiger
+ * Return: 0
+ */
+
+
+void print_number(int n)
+{
+unsigned int r = n;
+
+if (n < 0)
+{
+	n *= -1;
+_putchar('-');
+}
+r /= 10;
+if (r != 0)
+print_number(k);
+_putchar((unsigned int) n % 10 + '0');
+
+}
