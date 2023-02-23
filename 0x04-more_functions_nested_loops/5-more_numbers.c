d9184e1268c0aade7eabce8d157f1e1824768fb5
rob <robelmidedso@gmail.com>
2023-02-23 13:17:42 +0000

    c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,16 @@ void more_numbers(void)
 {
 int c;
 int r;
-int s;
-
 for (r = 0; r < 10; r++)
-
 {
-
-for (c = 48; c <= 57; c++)
+for (c = 0; c <= 14; c++)
 {
-	for (s = "LF"; s <= "SO"; s++)
-_putchar(c);
-	_putchar(s);
+if (c > 9)
+{
+_putchar((c / 10) + '0');
+}
+_putchar((c % 10) + '0');
+}
 _putchar('\n');
 }
-}
-
 }
