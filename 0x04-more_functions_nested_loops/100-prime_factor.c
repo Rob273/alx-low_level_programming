c0ec0a3d566ee668ec9467debf9c144750d958c4
rob <robelmidedso@gmail.com>
2023-02-23 17:54:45 +0000

    c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints the largest prime factor of the number
+ * Return: 0
+ */
+int main(void)
+{
+long x, maxf;
+long n = 612852475143;
+for (x = 1; x <= square; x++)
+{
+if (n % x == 0)
+{
+maxf = n / x;
+}
+}
+printf("%ld\n", maxf);
+return (0);
+}
