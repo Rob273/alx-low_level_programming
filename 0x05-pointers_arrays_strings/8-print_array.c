71869c42e8926b4a6d486773ffe72085818f157d
rob <robelmidedso@gmail.com>
2023-02-27 17:19:55 +0000

    pointer

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_array - print an array
+ * @a: string
+ * @n: intiger
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0; i < n; i++)
+{
+
+printf("a[n]");
+printf(",");
+printf(" ");
+}
+
+}
+
+
