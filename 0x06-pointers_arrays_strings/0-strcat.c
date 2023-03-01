9e71b5c0da8aac1356ae2ec12a6ae23df5803042
rob <robelmidedso@gmail.com>
2023-03-01 12:00:40 +0000

    string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *_strcat - concatenates two strings
+ *@dest: first string
+ *@src: second string
+ *Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest[] += src[]
+}
