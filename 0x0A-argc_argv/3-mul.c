af190070459d644658e0a7f2b46041fc9c976acf
rob <robelmidedso@gmail.com>
2023-03-11 08:28:05 +0000

    o

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - multiplies tow num
+ * @argc: arg
+ * @argv: array of arg
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int mul, i, r;
+
+if (argc != 3)
+{
+	 printf("Error\n");
+	 return (1);
+}
+	i = atoi(argv[1]);
+	r = atoi(argv[2]);
+	mul = i * r;
+	printf("%d\n", mul);
+return (0);
+}
