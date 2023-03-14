29966364d781dad2438b3684e221f802dada7912
rob <robelmidedso@gmail.com>
2023-03-14 07:04:31 +0000

    r

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,13 +18,13 @@ if (size == 0)
 return (NULL);
 }
 array = malloc(sizeof(char) * size);
+if (array == NULL)
+{
+	return (NULL);
+}
 for (i = 0; i < size; i++)
 {
 array[i] = c;
-}
-if (array == NULL)
-{
-return (NULL);
 }
 return (array);
 }
